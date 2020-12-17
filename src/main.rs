206a031bcf5092709cec580e6a7c6a0bf19bb4a4
Andrea <andrea.fasola@gmail.com>
2020-12-17 11:59:10 +0100

    Added custom test framework

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 #![no_std]
 #![no_main]
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(cpos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use cpos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Questa linea e' stampata con la mia println! macro --> {}", "println!(\"Questa linea e' stampata con la mia println! macro --> {}\", \"\");");
-    panic!("Spacco tutto!!!! {}", "panic!(\"Spacco tutto!!!! {}\", \"\");");
+    println!("Hello Cinepizza!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    cpos::test_panic_handler(info)
+}
